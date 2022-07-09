6ddd344579ed4b691eab3bc5fd62e7e31b8dab6f
Sébastien Vallet <sebastien.vallet89@gmail.com>
2022-07-09 12:31:21 +0200

    added prometheus and grafana

--- src/main/java/com/trodix/keycloakdemo/market/service/ProductService.java
+++ src/main/java/com/trodix/keycloakdemo/market/service/ProductService.java
@@ -2,7 +2,6 @@ package com.trodix.keycloakdemo.market.service;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
-
 import com.trodix.keycloakdemo.core.performancelogging.annotation.PerformanceLogging;
 import com.trodix.keycloakdemo.market.entity.Product;
 import com.trodix.keycloakdemo.market.mapper.ProductMapper;
