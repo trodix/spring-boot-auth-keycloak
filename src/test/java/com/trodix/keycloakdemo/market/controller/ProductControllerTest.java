a8f1e4b4230da2b6cfbc0329657fe01f1b977a4b
Sébastien Vallet <sebastien.vallet89@gmail.com>
2022-10-30 14:49:12 +0100

    fix tests

--- src/test/java/com/trodix/keycloakdemo/market/controller/ProductControllerTest.java
+++ src/test/java/com/trodix/keycloakdemo/market/controller/ProductControllerTest.java
@@ -1,68 +1,67 @@
-// package com.trodix.keycloakdemo.market.controller;
+package com.trodix.keycloakdemo.market.controller;
 
-// import static org.mockito.ArgumentMatchers.any;
-// import static org.mockito.Mockito.when;
-// import java.util.ArrayList;
-// import java.util.List;
-// import org.assertj.core.api.Assertions;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.extension.ExtendWith;
-// import org.mapstruct.factory.Mappers;
-// import org.mockito.Mock;
-// import org.mockito.junit.jupiter.MockitoExtension;
-// import com.trodix.keycloakdemo.market.dto.request.CreateProductRequest;
-// import com.trodix.keycloakdemo.market.dto.response.ProductResponse;
-// import com.trodix.keycloakdemo.market.mapper.ProductMapper;
-// import com.trodix.keycloakdemo.market.model.ProductModel;
-// import com.trodix.keycloakdemo.market.service.ProductService;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import com.trodix.keycloakdemo.market.dto.request.CreateProductRequest;
+import com.trodix.keycloakdemo.market.dto.response.ProductResponse;
+import com.trodix.keycloakdemo.market.mapper.ProductMapper;
+import com.trodix.keycloakdemo.market.model.ProductModel;
+import com.trodix.keycloakdemo.market.service.ProductService;
 
-// @ExtendWith(MockitoExtension.class)
-// public class ProductControllerTest {
+@ExtendWith(MockitoExtension.class)
+public class ProductControllerTest {
 
-//     private ProductMapper productMapper;
+    private ProductMapper productMapper;
 
-//     @Mock
-//     ProductService productService;
+    @Mock
+    ProductService productService;
 
-//     private ProductController productController;
+    private ProductController productController;
 
-//     @BeforeEach
-//     void initUseCase() {
-//         productMapper = Mappers.getMapper(ProductMapper.class);
-//         productController = new ProductController(productService, productMapper);
-//     }
+    @BeforeEach
+    void initUseCase() {
+        productMapper = Mappers.getMapper(ProductMapper.class);
+        productController = new ProductController(productService, productMapper);
+    }
 
-//     @Test
-//     public void save_product_success() throws Exception {
-//         final ProductModel productModel = new ProductModel(1L, "Tomates", "Du jardin");
+    @Test
+    public void save_product_success() throws Exception {
+        final ProductModel productModel = new ProductModel(1L, "Tomates", "Du jardin");
 
-//         final CreateProductRequest request = new CreateProductRequest();
-//         request.setName("Tomates");
-//         request.setDescription("Du jardin");
+        final CreateProductRequest request = new CreateProductRequest();
+        request.setName("Tomates");
+        request.setDescription("Du jardin");
 
-//         when(productService.saveProduct(any(ProductModel.class))).thenReturn(productModel);
+        when(productService.saveProduct(any(ProductModel.class))).thenReturn(productModel);
 
-//         final ProductResponse response = productController.createProduct(request);
-//         Assertions.assertThat(response.id()).isEqualTo(1L);
-//         Assertions.assertThat(response.name()).isEqualTo("Tomates");
-//         Assertions.assertThat(response.description()).isEqualTo("Du jardin");
-//     }
+        final ProductResponse response = productController.createProduct(request);
+        Assertions.assertThat(response.getId()).isEqualTo(1L);
+        Assertions.assertThat(response.getName()).isEqualTo("Tomates");
+        Assertions.assertThat(response.getDescription()).isEqualTo("Du jardin");
+    }
 
-//     @Test
-//     public void product_fetch_in_db_success() throws Exception {
-//         final List<ProductModel> productModelList = new ArrayList<>();
-//         productModelList.add(new ProductModel(1L, "Tomates", "Du jardin"));
-//         productModelList.add(new ProductModel(2L, "Camenbert", "Fort et coulant"));
+    @Test
+    public void product_fetch_in_db_success() throws Exception {
+        final List<ProductModel> productModelList = new ArrayList<>();
+        productModelList.add(new ProductModel(1L, "Tomates", "Du jardin"));
 
-//         final CreateProductRequest request = new CreateProductRequest();
-//         request.setName("Tomates");
-//         request.setDescription("Du jardin");
+        final CreateProductRequest request = new CreateProductRequest();
+        request.setName("Tomates");
+        request.setDescription("Du jardin");
 
-//         when(productService.getAllProducts()).thenReturn(productModelList);
+        when(productService.findProductByNameOrDescriptionContaining("Tomates", 0)).thenReturn(productModelList);
 
-//         final List<ProductResponse> response = productController.getProducts();
-//         Assertions.assertThat(response.size()).isEqualTo(2);
-//     }
+        final List<ProductResponse> response = productController.searchProducts("Tomates", 0).getItems();
+        Assertions.assertThat(response.size()).isEqualTo(1);
+    }
 
-// }
+}
--- src/test/java/com/trodix/keycloakdemo/market/service/ProductServiceTest.java
+++ src/test/java/com/trodix/keycloakdemo/market/service/ProductServiceTest.java
@@ -1,67 +1,83 @@
-// package com.trodix.keycloakdemo.market.service;
+package com.trodix.keycloakdemo.market.service;
 
-// import static org.mockito.ArgumentMatchers.any;
-// import static org.mockito.Mockito.when;
-// import java.util.ArrayList;
-// import java.util.List;
-// import org.assertj.core.api.Assertions;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.extension.ExtendWith;
-// import org.mapstruct.factory.Mappers;
-// import org.mockito.Mock;
-// import org.mockito.junit.jupiter.MockitoExtension;
-// import com.trodix.keycloakdemo.market.entity.Product;
-// import com.trodix.keycloakdemo.market.mapper.ProductMapper;
-// import com.trodix.keycloakdemo.market.model.ProductModel;
-// import com.trodix.keycloakdemo.market.repository.ProductRepository;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import com.trodix.keycloakdemo.market.entity.Product;
+import com.trodix.keycloakdemo.market.entity.ProductIndex;
+import com.trodix.keycloakdemo.market.mapper.ProductMapper;
+import com.trodix.keycloakdemo.market.model.ProductModel;
+import com.trodix.keycloakdemo.market.repository.ProductRepository;
 
-// @ExtendWith(MockitoExtension.class)
-// public class ProductServiceTest {
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceTest {
 
-//     @Mock
-//     private ProductRepository productRepository;
+    @Mock
+    private ProductRepository productRepository;
 
-//     @Mock
-//     private JmsProducer jmsProducer;
+    @Mock
+    private ProductSearchService productSearchService;
 
-//     private ProductMapper productMapper;
+    @Mock
+    private ProductIndexerService productIndexerService;
 
-//     ProductService productService;
+    @Mock
+    private JmsProducer jmsProducer;
 
-//     @BeforeEach
-//     void initUseCase() {
-//         productMapper = Mappers.getMapper(ProductMapper.class);
-//         productService = new ProductService(productRepository, productMapper, jmsProducer);
-//     }
+    private ProductMapper productMapper;
 
-//     @Test
-//     public void savedProduct_Success() {
-//         final Product product = new Product(1L, "Tomates", "Du jardin");
-//         final ProductModel productModel = new ProductModel(null, "Tomates", "Du jardin");
+    ProductService productService;
 
-//         // providing knowledge
-//         when(productRepository.saveAndFlush(any(Product.class))).thenReturn(product);
+    @BeforeEach
+    void initUseCase() {
+        productMapper = Mappers.getMapper(ProductMapper.class);
+        productService = new ProductService(productRepository, productSearchService, productIndexerService, productMapper, jmsProducer);
+    }
 
-//         final ProductModel savedProduct = productService.saveProduct(productModel);
+    @Test
+    public void savedProduct_Success() {
+        final Product product = new Product(1L, "Tomates", "Du jardin");
+        final ProductModel productModel = new ProductModel(null, "Tomates", "Du jardin");
 
-//         Assertions.assertThat(savedProduct.getProductId()).isNotNull();
-//         Assertions.assertThat(savedProduct.getProductId()).isEqualTo(1L);
-//         Assertions.assertThat(savedProduct.getName()).isNotBlank();
-//         Assertions.assertThat(savedProduct.getDescription()).isNotBlank();
-//     }
+        // providing knowledge
+        when(productRepository.saveAndFlush(any(Product.class))).thenReturn(product);
 
-//     @Test
-//     public void product_exists_in_db_success() {
-//         final Product product = new Product(1L, "Tomates", "Du jardin");
-//         final List<Product> productList = new ArrayList<>();
-//         productList.add(product);
+        final ProductModel savedProduct = productService.saveProduct(productModel);
 
-//         // providing knowledge
-//         when(productRepository.findAll()).thenReturn(productList);
+        Assertions.assertThat(savedProduct.getProductId()).isNotNull();
+        Assertions.assertThat(savedProduct.getProductId()).isEqualTo(1L);
+        Assertions.assertThat(savedProduct.getName()).isNotBlank();
+        Assertions.assertThat(savedProduct.getDescription()).isNotBlank();
+    }
 
-//         final List<ProductModel> fetchedProducts = productService.getAllProducts();
-//         Assertions.assertThat(fetchedProducts.size()).isGreaterThan(0);
-//     }
+    @Test
+    public void product_exists_in_db_success() {
+        final Product product = new Product(1L, "Tomates", "Du jardin");
+        final List<Product> productList = new ArrayList<>();
+        productList.add(product);
 
-// }
+        final ProductIndex productIndex = new ProductIndex();
+        productIndex.setId(1L);
+        productIndex.setName("Tomates");
+        productIndex.setDescription("Du jardin");
+
+        final List<ProductIndex> productIndexList = new ArrayList<>();
+        productIndexList.add(productIndex);
+
+        // providing knowledge
+        when(productSearchService.findByNameOrDescriptionContaining("Tomates", 0)).thenReturn(productIndexList);
+        when(productRepository.findAllById(List.of(1L))).thenReturn(productList);
+
+        final List<ProductModel> fetchedProducts = productService.findProductByNameOrDescriptionContaining("Tomates", 0);
+        Assertions.assertThat(fetchedProducts.size()).isGreaterThan(0);
+    }
+
+}
